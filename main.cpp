835b95278c8a8f178947f982eef499a1d13f8f73
glebuben <glebenbuben@gmail.com>
2024-04-07 18:27:17 +0300

    Написал вывод нужной информации, полученной с хаптика

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <HD/hd.h>
+#include <Cursor.h>
 #include <HDU/hduError.h>
 #include <HDU/hduVector.h>
 #include "server.h"
@@ -36,20 +37,76 @@ struct msg
 };
 msg mymsg;
 int command = mymsg.command;
-HDCallbackCode HDCALLBACK positionCallback(void *userData)
+HDCallbackCode HDCALLBACK informationCallback(void *userData)
 {
     hdBeginFrame(hdGetCurrentDevice());
 
-    hduVector3Dd position;
+    // Текущее положение стилуса
+    HDint buttonState;
+    HDboolean safetySwitch;
+    HDboolean inkwellSwitch;
+    hduVector3Dd position; // позиция
+    hduVector3Dd joint_angles; // углы шарниров основания
+    hduVector3Dd joint_gimbal_angles; // углы шарниров карданового подвеса (последних трёх шариниров)
+    HDdouble transform[16]; // матрица преобразования
+    HDlong encoder_values[6];
+
+//    std::cout << "Current buttons state:" << transform << std::endl;
+
+    // Получение данных
     hdGetDoublev(HD_CURRENT_POSITION, position);
+    hdGetDoublev(HD_CURRENT_JOINT_ANGLES, joint_angles);
+    hdGetDoublev(HD_CURRENT_GIMBAL_ANGLES, joint_gimbal_angles);
+    hdGetDoublev(HD_CURRENT_TRANSFORM, transform);
+    hdGetIntegerv(HD_CURRENT_BUTTONS, &buttonState);
+    hdGetBooleanv(HD_CURRENT_SAFETY_SWITCH, &safetySwitch);
+    hdGetBooleanv(HD_CURRENT_INKWELL_SWITCH, &inkwellSwitch);
+    hdGetLongv(HD_CURRENT_ENCODER_VALUES, encoder_values);
+
+    // Состояние кнопок. Каждый бит соответствует определенной кнопке.
+    std::cout << "Current buttons state:" << buttonState <<  std::endl;
+    // Чтобы получать значение с определенной кнопки можно использовать побитовое 'и' с нужной кнопкой.
+    // Значение для каждой кнопки - 2 в какой-то степени, в нашем случае это просто '1' и '2'
+    HDint button1 = buttonState & 1;
+    std::cout  << "Current button 1 state:"<< button1 <<  std::endl;
+    HDint button2 = buttonState & 2;
+    std::cout  << "Current button 2 state:"<< button2 <<  std::endl;
+
+//    // Проверка предохранительный выключатель (safety switch)
+//    // У нашего устройства его либо нет, либо я не разобрался, где его найти. Всегда '0'
+//    std::cout << static_cast<int>(safetySwitch) <<  std::endl;
+
+    // Проверка, что стилус находится в чернильнице. Если внутри, возвращает '0', в обратном случае '1'
+    std::cout << static_cast<int>(inkwellSwitch) <<  std::endl;
+
+//    std::cout << "Current Encoder values:" <<  std::endl
+//    << encoder_values[0] << ", "
+//    << encoder_values[1] << ", "
+//    << encoder_values[2] << ", "
+//    << encoder_values[3] << ", "
+//    << encoder_values[4] << ", "
+//    << encoder_values[5] << std::endl;
+
+//    // Матрица преобразования. Не уверен, в каком порядке нужно выводить значения
+//    std::cout << "Current Transformation matrix:" <<  std::endl
+//    << transform[0] << ", " << transform[4] << ", " << transform[8] << ", " << transform[12] << std::endl
+//    << transform[1] << ", " << transform[5] << ", " << transform[9] << ", " << transform[13] << std::endl
+//    << transform[2] << ", " << transform[6] << ", " << transform[10] << ", " << transform[14] << std::endl
+//    << transform[3] << ", " << transform[7] << ", " << transform[11] << ", " << transform[15] << std::endl;
+
+
+//    // Углы первых трёх шарниров
+//    std::cout << "Current base angles: ("
+//              << joint_angles << ")"
+//              << std::endl;
+
+//    // Углы последних трёх шарниров
+//    std::cout << "Current position: ("
+//            << joint_gimbal_angles << ")"
+//            << std::endl << std::endl;
+//
 
     hdEndFrame(hdGetCurrentDevice());
-
-    std::cout << "Current position: ("
-              << position[0] << ", "
-              << position[1] << ", "
-              << position[2] << ")"
-              << std::endl;
 
     return HD_CALLBACK_CONTINUE;
 }
@@ -66,11 +123,6 @@ HDCallbackCode HDCALLBACK forceCallback(void *userData)
     // притягиваем стилус к началу координат.
     // Пример: простая пропорциональная сила, направленная к центру.
     hduVector3Dd force;
-//    std::cout << "Current position: ("
-//              << position[0] << ", "
-//              << position[1] << ", "
-//              << position[2] << ")"
-//              << std::endl;
     if(mymsg.command == 65538) {
         force[0] = -position[0] * 0.02; // Коэффициент управляет "жесткостью" силы
         force[1] = -position[1] * 0.02;
@@ -105,7 +157,7 @@ int main()
     }
 
 //    HDulong callbackHandle = hdScheduleAsynchronous(positionCallback, NULL, HD_DEFAULT_SCHEDULER_PRIORITY);
-    HDulong callbackHandle = hdScheduleAsynchronous(forceCallback, NULL, HD_DEFAULT_SCHEDULER_PRIORITY);
+    HDulong callbackHandle = hdScheduleAsynchronous(informationCallback, NULL, HD_DEFAULT_SCHEDULER_PRIORITY);
 
     std::cout << "Press Enter to quit..." << std::endl;
     std::cin.get();
